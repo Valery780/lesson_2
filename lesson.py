6be27c6cb3747b9946fc71f9c0e0227dff9e9e5c
Valery780 <70340887+Valery780@users.noreply.github.com>
2020-09-02 13:00:32 +0300

    Add files via upload

--- lesson.py
+++ lesson.py
@@ -1,1 +1,8 @@
-print('Hello, ' + str('Kitty') + '!')
+if __name__ == "__main__":
+    x= int(input("Enter num: "))
+    if x >0:
+        print("sign(x)=1")
+    elif x <0:
+        print("sign(x)=-1")
+    else:
+        print("sign(x)=0")
